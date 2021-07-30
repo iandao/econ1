a46ddad406c7fec1ce23e15ed13bb24b41a8f4a6
ian dao <ian.dao@pm.me>
2021-07-30 16:52:04 +0200

    gitignore added

--- app/__init__.py
+++ app/__init__.py
@@ -25,22 +25,3 @@ def create_app(config_class=Config):
 
 from app import models
 
-
-
-# app = Flask(__name__)
-# app.config.from_object(Config)
-
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-# bootstrap = Bootstrap(app)
-
-# app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
-#     if app.config['ELASTICSEARCH_URL'] else None
-
-
-# from app import models, routes
-
-
-
-
-
